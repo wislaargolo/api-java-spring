f97363489f2c73ba7c2f481082224a7570243147
Wisla Argolo <wislaargolo@Wislas-MacBook-Pro.local>
2024-03-08 11:23:21 -0300

    criacao de repository produto

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,16 +1,31 @@
 package com.example.crud.controllers;
 
+import com.example.crud.domain.product.ProductRepository;
+import com.example.crud.domain.product.RequestProduct;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public ResponseEntity getAllProducts() {
-        return ResponseEntity.ok("deu ok");
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity registerProduct(@RequestBody @Valid RequestProduct data) {
+        return ResponseEntity.ok().build();
     }
 }
---
+++ src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -0,0 +1,4 @@
+package com.example.crud.domain.product;
+
+public record RequestProduct(String name, Integer price_in_cents) {
+}
