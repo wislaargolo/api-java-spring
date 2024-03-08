a9efb045a411bfbadc4ed97cf25edaecc08edf80
Wisla Argolo <wislaargolo@Wislas-MacBook-Pro.local>
2024-03-08 10:17:06 -0300

    configuracoes para migration e criacao de entidade produto

---
+++ src/main/java/com/example/crud/domain/product/Product.java
@@ -0,0 +1,21 @@
+package com.example.crud.domain.product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name="product")
+@Entity(name="product")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+}
